60c092cd9a704676246b62c2f4cf1d4a08882cb0
Rodrigo Quesada <rodrigoquesada@gmail.com>
2015-11-17 19:00:03 -0600

    Converted into Android project.

--- src/stuffTest/java/sample/StuffTest.kt
+++ src/stuffTest/java/sample/StuffTest.kt
@@ -1,12 +1,11 @@
 package sample
 
 import org.junit.Test
-import sample.Stuff
 
 /**
  * Created by Rodrigo Quesada on 02/10/15.
  */
-class StuffTest {
+internal class StuffTest {
 
     @Test
     fun testDoSomething() {
